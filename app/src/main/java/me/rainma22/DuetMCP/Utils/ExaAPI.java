d4a5be8eaf88f949557a61a81131f81d7d2f7e99
RainMa22 <d2rain@126.com>
2026-07-04 15:27:31 -0700

    remove search and api features in favor of the use of plugins

--- app/src/main/java/me/rainma22/DuetMCP/Utils/ExaAPI.java
+++
@@ -1,7 +0,0 @@
-package me.rainma22.DuetMCP.Utils;
-
-public class ExaAPI {
-    
-    public static final String URL = "https://api.exa.ai/";
-    public static final String VENDOR_NAME = "exa";
-}
