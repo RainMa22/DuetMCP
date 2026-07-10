57e58ec46df35eb24d17fd482c8de7d1d11134b6
RainMa22 <d2rain@126.com>
2026-07-09 18:42:44 -0700

    extend fetch to block local ip access when enabled

--- app/src/main/java/me/rainma22/DuetMCP/Tools/InternetTools/fetch/DirectFetchTool.java
+++ app/src/main/java/me/rainma22/DuetMCP/Tools/InternetTools/fetch/DirectFetchTool.java
@@ -1,6 +1,7 @@
 package me.rainma22.DuetMCP.Tools.InternetTools.fetch;
 
 import java.io.IOException;
+import java.net.InetAddress;
 import java.net.URI;
 import java.util.List;
 import java.util.Map;
@@ -14,22 +15,42 @@ import org.json.JSONObject;
  */
 public class DirectFetchTool extends InternetFetchTool {
 
-        public DirectFetchTool(JSONObject conf) {
-            super(conf);
-        }
+    private final System.Logger LOGGER = System.getLogger(this.getClass().getName());
 
-        public JSONObject apply(UserContext uctx, JSONObject arguments) throws BadRequestException {
-            try {
-                String url = arguments.optString("url");
-                String res = Downloader.get(URI.create(url));
-                return new JSONObject(Map.of(
-                        "content", List.of(
-                                Map.of("type", "text",
-                                        "text", res)
-                        )
-                ));
-            } catch (IOException | InterruptedException ex) {
-                throw new BadRequestException(ex);
+    public DirectFetchTool(JSONObject conf) {
+        super(conf);
+    }
+
+    public JSONObject apply(UserContext uctx, JSONObject arguments) throws BadRequestException {
+        try {
+            String url = arguments.optString("url");
+            LOGGER.log(System.Logger.Level.INFO, "received url: " + url);
+            if (url == null || url.isEmpty()) {
+                throw new BadRequestException("URL cannot be null, undefined or empty.");
             }
+            if (!url.toLowerCase().startsWith("http://") && !url.toLowerCase().startsWith("https://")) {
+                LOGGER.log(System.Logger.Level.INFO, "received padding url with \"http://\"");
+                url = "http://" + url;
+            }
+            URI uri = URI.create(url);
+            if (conf.optBoolean("allow local ip access", false) == false) {
+                InetAddress addr = InetAddress.getByName(uri.getHost());
+                if (addr.isSiteLocalAddress()) {
+                    LOGGER.log(System.Logger.Level.INFO, "blocked GET request to address: " + addr.toString());
+
+                    throw new BadRequestException("Local address access not allowed by the MCP host.\n"
+                            + "If you think this is a mistake, please contact the MCP host");
+                }
+            }
+            String res = Downloader.get(uri);
+            return new JSONObject(Map.of(
+                    "content", List.of(
+                            Map.of("type", "text",
+                                    "text", res)
+                    )
+            ));
+        } catch (IOException | InterruptedException ex) {
+            throw new BadRequestException(ex);
         }
     }
+}
--- app/src/main/java/me/rainma22/DuetMCP/Tools/InternetTools/fetch/InternetFetchToolPlugin.java
+++ app/src/main/java/me/rainma22/DuetMCP/Tools/InternetTools/fetch/InternetFetchToolPlugin.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import me.rainma22.DuetMCP.Tools.ToolFactory;
 import me.rainma22.DuetMCP.Tools.ToolPlugin;
 import me.rainma22.DuetMCP.Utils.ConfigurationManager;
+import org.json.JSONObject;
 
 /**
  *
@@ -16,44 +17,24 @@ public class InternetFetchToolPlugin implements ToolPlugin {
     @Override
     public void onLoad() {
         final Map<String, Object> INFO = Map.of(
-                "name", "duet_InternetFetch",
+                "name", "duet_directInternetFetch",
                 "title", "Web Resource Fetching",
                 "description", "Fetch an internet resource using HTTP GET; \n"
-                + "Remember to prefix the url with http or https.\n"
-                + "When adding parameters to the url, "
                 + "make sure to URL-encode your parameters.",
                 "inputSchema", Map.of(
                         "type", "object",
-                        "properties", Map.of("url", Map.of("type", "string",
-                                "description", "URL of the web resource"))),
-                 "required", List.of("url")
+                        "properties", Map.of(
+                                "url", Map.of(
+                                        "type", "string",
+                                        "description", "URL of the web resource"),
+                                "header", Map.of(
+                                        "type", "object",
+                                        "description", "the header for the GET request"))),
+                "required", List.of("url")
         );
-//        final Map<String, Object> apiDefault = Map.of(
-//                "vendor", ExaAPI.VENDOR_NAME,
-//                "API_KEY", ""
-//        );
-        final Map<String, Object> downloadDefault = Map.of(
-                "type", "direct download"
-        );
-        final Map<String, Object> defaultConfig = Map.of(
-                "type", "direct download",
-                //                "api_setting", apiDefault,
-                "download_setting", downloadDefault
-        );
-        var conf = ConfigurationManager.ofClass(this.getClass()).getConfig(defaultConfig);
-        switch (conf.getString("type")) {
-//            case "api":
-//                ToolFactory.registerTool(INFO.get("name").toString(), INFO, new ApiFetchTool(conf.getJSONObject("api_setting")));
-//                break;
-            default:
-                logger.log(System.Logger.Level.WARNING, String.format("Unsupported type %s", conf.getString("type")));
-                logger.log(System.Logger.Level.WARNING, "defaulting to direct download...");
-            //fallthrough
-            case "direct download":
-                var downloadConf = conf.getJSONObject("download_setting");
-                ToolFactory.registerTool(INFO.get("name").toString(), INFO, new DirectFetchTool(downloadConf));
-                break;
-        }
+        final Map<String, Object> DEFAULT_CONFIG =
+                Map.of("allow local ip access", false);
+        var conf = ConfigurationManager.ofClass(this.getClass()).getConfig(DEFAULT_CONFIG);
+        ToolFactory.registerTool(INFO.get("name").toString(), INFO, new DirectFetchTool(conf));
     }
-
 }
